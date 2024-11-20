0e57e5ff2cefca1c85fcee85d6d9f01e11c01a7b
Matteo Valenzi <dilloawallace@hotmail.com>
2024-11-20 19:36:24 +0100

    pagine dinamiche

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,19 @@
 import { createServer, IncomingMessage, ServerResponse } from "node:http";
 import url from "node:url";
+import { readFileSync } from "node:fs";
+import path from "node:path";
 
 const requestListener = (req: IncomingMessage, res: ServerResponse) => {
     console.log("Richiesta ricevuta:", req.method, req.url, new Date().toLocaleString());
 
     const u = url.parse(req.url!, true);
+    const nome = u.query.nome as string;
+    const colore = u.query.colore as string;
 
     switch (u.pathname) {
         case "/":
             res.writeHead(200, { "Content-Type": "text/html" })
-            res.write("<html><head><title>Mio sito</title></head><body><h1>Home Page</h1></body></html>");
+            res.write("<html><head><title>Mio sito</title></head><body><h1>Ciao [[nome]]</h1></body></html>".replace("[[nome]]", nome ?? "a tutti"));
             res.end();
             break;
         case "/pippo":
@@ -21,8 +25,11 @@ const requestListener = (req: IncomingMessage, res: ServerResponse) => {
             res.end(JSON.stringify(pippo));
             break;
         case "/ciao":
-            res.write("Ciao!!!");
-            res.end();
+            let html = readFileSync(path.join("./templates", "welcome.html"), "utf8");
+            res.writeHead(200, { "Content-Type": "text/html" });
+            html = html.replace("[[nome]]", nome ?? "a te");
+            html = html.replace("[[colore]]", colore ?? "FF0000");
+            res.end(html);
             break;
         default:
             res.writeHead(404);
